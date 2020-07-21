77b0a35eb44b7f3a47c932f3ede3deae2b42a12b
Nick Costanza <nicholas.g.costanza@gmail.com>
2020-07-21 13:58:23 -0700

    functioning query from openweather for current weather. icon is not working, need to restructor html for functionality

--- script.js
+++ script.js
@@ -9,34 +9,37 @@
 
 
 
+
+$('#searchBtn').on('click', function() {
+
+var city = $('#city').val().trim();    
 var apiKey = 'fe4de04b2dc40cad6dc883405a9a210c';
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=Denver" + "&appid=" + apiKey;
-var city = $('#city').val().trim();
-
-$(document).ready();
+var queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial&appid=' + apiKey;
 
 
-
-console.log(queryURL)
-
-$('#searchBtn').on('click', function(){
-
-var apiKey = 'fe4de04b2dc40cad6dc883405a9a210c';
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
-var city = $('#city').val().trim();
 
     $.ajax({
         url: queryURL,
         method: 'GET'
     }).then(function(response){
-        $('.cityName').html('<h2>' + response.name + "Weather Details</h1>" );
-        $('.temp').text('Temperature: ' + response.main.temp);
+
+    
+        var icon = response.weather.icon;
+
+        console.log(icon);
+
+
+    
+        $('#wicon').attr('src', icon);   
+        $('.cityName').html('<h2>' + response.name + "</h1>" );
+        $('.temp').text(response.main.temp +'°F');
         $('.humidity').text('Humidity: ' + response.main.humidity);
-        $('.wind').text('Wind Speed: ' + response.wind.speed);
+        $('.wind').text('Wind Speed: ' + response.wind.speed + ' MPH');
         $('uvIndex').text('UV Index: ');
+        
     });
 
-    console.log(response.main.temp);
+    
 
 });
  
